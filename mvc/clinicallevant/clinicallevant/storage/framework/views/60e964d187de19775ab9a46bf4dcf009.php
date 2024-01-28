a505dc8518515bc61d15e8d6c4fcd6b119f27ea9
Danrodriguez04 <danielsantiagorodriguez72@gmail.com>
2024-01-28 01:09:18 +0100

    eliminando ficheros no importantes

--- mvc/clinicallevant/clinicallevant/storage/framework/views/60e964d187de19775ab9a46bf4dcf009.php
+++
@@ -1,4 +0,0 @@
-<h1>¿Que tipo de cita quieres?</h1>
-
-
-<?php echo $__env->make('layouts', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/clinica/clinicallevant/resources/views/layouts/cita/tipo.blade.php ENDPATH**/ ?>
--- mvc/clinicallevant/clinicallevant/storage/framework/views/68f996eed2e03a240e2df90829b8687b.php
+++
@@ -1,9 +0,0 @@
-<!-- //desde nuestra plantilla cita cambiamos el contenido que queremos cambiar-->
-<?php $__env->startSection('title', 'Lista de citas'); ?>
-<?php $__env->startSection('content'); ?>
-
-
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.cita', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/clinica/clinicallevant/resources/views/layouts/cita/showcita.blade.php ENDPATH**/ ?>
